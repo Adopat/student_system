77a395489874f3af596623cd43d215c5d2cb8595
Adopat <1615581829@qq.com>
2023-02-21 16:56:32 +0800

    增加班级的修改

--- src/com/xxx/sms/controller/classes/EditServlet.java
+++ src/com/xxx/sms/controller/classes/EditServlet.java
@@ -1,5 +1,8 @@
 package com.xxx.sms.controller.classes;
 
+import com.xxx.sms.model.Classes;
+import com.xxx.sms.service.impl.ClassesServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,6 +20,17 @@ public class EditServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        // 1. POST 请求设置编码格式，解决获取中文乱码的问题
+        req.setCharacterEncoding("utf-8");
+        int id = Integer.parseInt(req.getParameter("id"));
+        String name = req.getParameter("name");
+        Classes classes = new Classes();
+        classes.setId(id);
+        classes.setName(name);
+        ClassesServiceImpl classesService = new ClassesServiceImpl();
+        boolean updateResult = classesService.update(classes);
+        if (updateResult) {
+            resp.sendRedirect(req.getContextPath()+"/classes/list");
+        }
     }
 }
