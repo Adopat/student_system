370a58d874445c0aca856002d836942a587de307
Adopat <1615581829@qq.com>
2023-02-21 16:54:36 +0800

    增加班级

--- src/com/xxx/sms/controller/classes/AddServlet.java
+++ src/com/xxx/sms/controller/classes/AddServlet.java
@@ -1,5 +1,9 @@
 package com.xxx.sms.controller.classes;
 
+import com.xxx.sms.dao.impl.ClassesDaoImpl;
+import com.xxx.sms.model.Classes;
+import com.xxx.sms.service.impl.ClassesServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +20,18 @@ public class AddServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        // 1. POST 请求设置编码格式，解决获取中文乱码的问题
+        req.setCharacterEncoding("utf-8");
+        int id = Integer.parseInt(req.getParameter("id"));
+        String name = req.getParameter("name");
+        Classes classes = new Classes();
+        classes.setId(id);
+        classes.setName(name);
+        ClassesServiceImpl classesService = new ClassesServiceImpl();
+        boolean addResult = classesService.add(classes);
+        // 3. 删除成功，重定向到学生列表页面
+        if (addResult) {
+            resp.sendRedirect(req.getContextPath()+"/classes/list");
+        }
     }
 }
