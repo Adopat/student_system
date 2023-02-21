8ff6c4a05fb97e83e7cf914f1973b72f572a670d
Adopat <1615581829@qq.com>
2023-02-21 16:56:22 +0800

    增加班级的删除

--- src/com/xxx/sms/controller/classes/DeleteServlet.java
+++ src/com/xxx/sms/controller/classes/DeleteServlet.java
@@ -1,5 +1,9 @@
 package com.xxx.sms.controller.classes;
 
+import com.xxx.sms.service.StudentService;
+import com.xxx.sms.service.impl.ClassesServiceImpl;
+import com.xxx.sms.service.impl.StudentServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,11 +15,19 @@ import java.io.IOException;
 public class DeleteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        doPost(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        // 1. 获取 id
+        String id = req.getParameter("id");
+        ClassesServiceImpl classesService = new ClassesServiceImpl();
+        // 2. 调用 studentService 的 delete 方法，删除学生信息
+        boolean delResult = classesService.delete(Integer.parseInt(id));
+        // 3. 删除成功，重定向到学生列表页面
+        if (delResult) {
+            resp.sendRedirect(req.getContextPath()+"/classes/list");
+        }
     }
 }
